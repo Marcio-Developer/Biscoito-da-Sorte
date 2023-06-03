98eddb9fe2fc5541bee6a4803581b24b8113691c
Marcio Silva <marciobolari@hotmail.com>
2023-06-03 00:01:46 -0300

    Novos conteúdos adicionados

--- src/App.js
+++ src/App.js
@@ -18,9 +18,19 @@ class App extends Component{
   'O sábio nunca diz tudo o que pensa, mas pensa sempre tudo o que diz', 
   'Deixe de lado as preocupações e seja feliz.',
   'Realize o óbvio, pense no improvável e conquiste o impossível.',
-  'Acredite em milagres, mas não dependa deles.',
+  'O objetivo de uma discussão não deve ser a vitória mas sim o progresso.',
   'A maior barreira para o sucesso é o medo do fracasso.',
+  'Precisamos evoluir e entender que pensar diferente não nos torna inimigos.',
+  'Onde dialogam pessoas sábias, pensamentos diferentes não geram conflitos, geram novas ideias.',
+  'Bons hábitos são construídos na base da disciplina e não da motivação.',
+  'A busca pela excelência não deve ser um objetivo mas sim um hábito.',
+  'Sofremos mais na imaginação do que na realidade.',
   'Mais graves são as consequências da raiva do que as suas causas.',
+  'Enquanto você vive, continue aprendendo a viver.',
+  'Se palavras te controlam, qualquer um pode te controlar.',
+  'Maturidade é saber ficar em silêncio, messmo quando temos muito a dizer.',
+  'Cada fracasso nos ensina algo que precisamos aprender.',
+  'Ter sucesso não é não fracassar...É não desistir apesar dos fracassos'
 ];
 
 } 
@@ -35,6 +45,7 @@ quebraBiscoito(){
 render(){
     return(
       <div className="container">
+        <h1>Biscoito da Sorte</h1>
         <img src={require('./assets/Biscoito.png')} className="img" />
         <Botao nome="Abrir Biscoito" acaoBtn={this.quebraBiscoito}/>
         <h3 className="textoFrase">{this.state.textoFrase}</h3>
